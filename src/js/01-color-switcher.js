80a4cf55c5613d4db07939811b7721a83a5e6eab
k09at1ch <newwerasik228@gmail.com>
2023-04-24 21:18:40 +0300

    1st and 2nd tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,37 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-  
-const timer=setInterval(getRandomHexColor(), 500)
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
+// const background=document.getElementsByTagName("body")
+// const startButton = document.querySelectorAll('[data-start]')[0];
+// const stopButton = document.querySelectorAll('[data-stop]')[0];
+// console.log(startButton)
+// const start=document.getElementById("start")
+// let timer=null
+// startButton.addEventListener("click", (event)=>{
+//     startButton.disabled=true
+//     timer=setInterval(()=>{
+//         background.style.backgroundcolor=getRandomHexColor()
+//         console.log(getRandomHexColor())
+//     }, 500)
+// })
 
-console.log(getRandomHexColor())
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let timer;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startButton.addEventListener('click', function() {
+  startButton.disabled = true;
+  timer = setInterval(function() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  clearInterval(timer);
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,52 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const datePicker = document.getElementById('datetime-picker');
+const startButton = document.getElementById('btn');
+const daysValue = document.getElementById('value1');
+const hoursValue = document.getElementById('value2');
+const minutesValue = document.getElementById('value3');
+const secondsValue = document.getElementById('value4');
+let arr=[]
+const dateSelector = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+  dateFormat: 'Y-m-d H:i',
+});
+startButton.addEventListener("click", (event)=>{
+    const selectedDate = dateSelector.selectedDates[0];
+    const now = new Date();
+    
+    if (selectedDate <= now) {
+      console.log('Please choose a date in the future.');
+      return;
+    }
+    const countdownDate = dateSelector.selectedDates[0];
+    countdownInterval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate.getTime() - now;
+  
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      daysValue.textContent = days.toString().padStart(2, '0');
+      hoursValue.textContent = hours.toString().padStart(2, '0');
+      minutesValue.textContent = minutes.toString().padStart(2, '0');
+      secondsValue.textContent = seconds.toString().padStart(2, '0');
+  
+      if (distance < 0) {
+        clearInterval(countdownInterval);
+        daysValue.innerHTML="00"
+        hoursValue.innerHTML="00"
+        minutesValue.innerHTML="00"
+        secondsValue.innerHTML="00" 
+      }
+    }, 1000);
+})
